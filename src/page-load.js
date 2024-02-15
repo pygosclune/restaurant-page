9fa610598c840ce410cfb20ae3318f3fd4b15c03
pygosclune <151787910+pygosclune@users.noreply.github.com>
2024-02-15 02:12:38 +0100

    added content for tabs

--- src/page-load.js
+++ src/page-load.js
@@ -41,6 +41,28 @@ export function loadMenu() {
     const h1Heading = document.createElement("h1");
     h1Heading.textContent = "Menu";
     mainDiv.appendChild(h1Heading);
+
+    const images = ['Oak', 'Beech', 'Pine', 'Mahogany'];
+    const images_src = ['oak-wood.jpg', 'beech-wood.jpg', 'pine-wood.jpg', 'mahogany-wood.jpg'];
+
+    for (let i = 0; i < images.length; i++) {
+        const pElement = document.createElement('p');
+        pElement.className = "wood-type";
+        pElement.textContent = images[i];
+        const image = document.createElement('img');
+        image.src = 'assets/' + images_src[i];
+
+        mainDiv.appendChild(pElement);
+        mainDiv.appendChild(image);
+    }
+
+    const aElement = document.createElement("a");
+    aElement.textContent = "Images from homesdirect365";
+    aElement.href = "https://www.homesdirect365.co.uk/blog/2020/07/a-guide-to-wood-types";
+    aElement.target = "_blank";
+    aElement.rel = "noopener noreferrer";
+    mainDiv.appendChild(aElement);
+
     contentDiv.appendChild(mainDiv);
 }
 
@@ -54,5 +76,9 @@ export function loadContact() {
     const h1Heading = document.createElement("h1");
     h1Heading.textContent = "Contact";
     mainDiv.appendChild(h1Heading);
+
+    const h3Heading = document.createElement("h3");
+    h3Heading.textContent = 'Find some tree around you and shout "restaurant" to contact us!';
+    mainDiv.appendChild(h3Heading);
     contentDiv.appendChild(mainDiv);
 }
